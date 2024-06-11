a1e8766a9e0abdb845dd17866c4f05e5f80b5695
hangjeff <163969474+hangjeff@users.noreply.github.com>
2024-06-11 14:53:40 +0800

    RoomController and SearchRoom modify

--- project_ver1/Controllers/RoomController.cs
+++ project_ver1/Controllers/RoomController.cs
@@ -30,37 +30,61 @@ namespace project_ver1.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> SearchRooms(string roomType, DateTime checkInDate, DateTime checkOutDate)
+        public ActionResult SearchRoom()
         {
-            // Query rooms based on room type and availability
-            var availableRooms = await _context.Rooms
-                .Where(r => r.Category.Name == roomType &&
-                            r.HasReserved == false &&
-                            r.CanReserve == true)
-                .ToListAsync();
+            //// Query rooms based on room type and availability
+            //var availableRooms = await _context.Rooms
+            //    .Where(r => r.Category.Name == roomType &&
+            //                r.HasReserved == false &&
+            //                r.CanReserve == true)
+            //    .ToListAsync();
 
-            // Filter available rooms based on check-in and check-out dates
-            foreach (var room in availableRooms.ToList())
-            {
-                var reservationsForRoom = await _context.RoomOrder
-                    .Where(ro => ro.ID == room.ID &&
-                                  ro.CheckIn <= checkOutDate &&
-                                  ro.CheckOut >= checkInDate)
-                    .ToListAsync();
+            //// Filter available rooms based on check-in and check-out dates
+            //foreach (var room in availableRooms.ToList())
+            //{
+            //    var reservationsForRoom = await _context.RoomOrder
+            //        .Where(ro => ro.ID == room.ID &&
+            //                      ro.CheckIn <= checkOutDate &&
+            //                      ro.CheckOut >= checkInDate)
+            //        .ToListAsync();
 
-                if (reservationsForRoom.Any())
-                {
-                    availableRooms.Remove(room);
-                }
-            }
+            //    if (reservationsForRoom.Any())
+            //    {
+            //        availableRooms.Remove(room);
+            //    }
+            //}
 
-            // Count the number of available rooms
-            int availableRoomCount = availableRooms.Count;
+            //// Count the number of available rooms
+            //int availableRoomCount = availableRooms.Count;
 
-            // Pass the number of available rooms to the view
-            ViewBag.AvailableRoomCount = availableRoomCount;
+            //// Pass the number of available rooms to the view
+            //ViewBag.AvailableRoomCount = availableRoomCount;
 
-            return PartialView("_SearchResultsPartial", availableRooms);
+
+            var rb = from rs in _context.Rooms
+                     join orderDetails in _context.RoomOrderDetails on rs.ID equals orderDetails.RoomID
+                     join order in _context.RoomOrder on orderDetails.OrderID equals order.ID
+                     where order.CheckIn > DateTime.Parse("2024-03-01 ")
+                        && order.CheckOut < DateTime.Parse("2024-05-11 ")
+                        //&& rs.CategoryID == 1
+                        //&& !_context.RoomOrderDetails.Any(rod => rod.RoomID == rs.ID)
+                        //&& !_context.RoomOrder.Any(ro =>
+                        //     ro.CheckIn <= DateTime.Parse("2024-04-11 ") &&
+                        //     ro.CheckOut >= DateTime.Parse("2024-04-01 "))
+
+                     select new
+                     {
+                         CategoryID = rs.CategoryID,
+                         CheckIn = order.CheckIn,
+                         CheckOut = order.CheckOut,
+                         ID = orderDetails.RoomID
+                     };
+
+            var RoombookingList = rb.ToList();
+
+
+
+            return View(RoombookingList);
         }
 
         private void SetUserViewBag()
