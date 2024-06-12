8f1e63071977e8651f1d35639a1cba464be21438
hangjeff <163969474+hangjeff@users.noreply.github.com>
2024-06-12 10:20:59 +0800

    modify RoomController SearchRooms()

--- project_ver1/obj/Debug/net8.0/project_ver1.AssemblyInfo.cs
+++ project_ver1/obj/Debug/net8.0/project_ver1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project_ver1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aa8a25587fb03dce01a1db04d538c51640ea43e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8615ec9bc1406693476ec814bce14d1a1361e45")]
 [assembly: System.Reflection.AssemblyProductAttribute("project_ver1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project_ver1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
