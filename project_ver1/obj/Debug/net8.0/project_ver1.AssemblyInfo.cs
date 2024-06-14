77f08ac86035b5c818056a23dc1bde9234a4d251
tim58786 <161785937+tim58786@users.noreply.github.com>
2024-06-14 09:37:27 +0800

    修改產品訂單

--- project_ver1/obj/Debug/net8.0/project_ver1.AssemblyInfo.cs
+++ project_ver1/obj/Debug/net8.0/project_ver1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project_ver1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5795fc8765ac7961442ccff0426c45d3e9bb99a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5828a0971469a12d4ccbe684afe83caba81ea83d")]
 [assembly: System.Reflection.AssemblyProductAttribute("project_ver1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project_ver1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
