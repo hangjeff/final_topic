e6d22bc03e2e3f42ca14b9aab440234b0b212ee5
hangjeff <163969474+hangjeff@users.noreply.github.com>
2024-06-14 11:52:40 +0800

    modify SearchRooms
    checkOutDate has the value now

--- project_ver1/obj/Debug/net8.0/project_ver1.AssemblyInfo.cs
+++ project_ver1/obj/Debug/net8.0/project_ver1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project_ver1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd5bbd63c8c2850a18d23c9d9dae166fca389da7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee0cfe47074b27d6c9a91b5ed892ec253717cb0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("project_ver1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project_ver1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
