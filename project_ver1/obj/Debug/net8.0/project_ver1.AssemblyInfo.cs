71f9d4201d8a082ddddc0b7e79e064d6626bed30
uuioojhg <161785782+uuioojhg@users.noreply.github.com>
2024-06-21 09:28:11 +0800

    員工後台訂房訂單新增日期搜尋

--- project_ver1/obj/Debug/net8.0/project_ver1.AssemblyInfo.cs
+++ project_ver1/obj/Debug/net8.0/project_ver1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project_ver1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7734834855aacba40074bda739a25fcb1e919e8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e405dd6879f5db57658e35d5d4b789102b59a950")]
 [assembly: System.Reflection.AssemblyProductAttribute("project_ver1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project_ver1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
